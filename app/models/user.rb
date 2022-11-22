affb13c3ea335ffce8da1ec8f1fa804aa1e35d8d
Manuel Sanchez <manuelsancheza@gmail.com>
2022-11-22 10:29:09 +0000

    Added Name and Bio to user registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validates :name, :email, presence: true
+  # validates :email, uniqueness: true
+  # validates :password, length: { in: 6..50 }
 end
