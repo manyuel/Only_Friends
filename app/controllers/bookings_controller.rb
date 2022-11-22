ba4f196d710f7e601b5af12e688ba31bd28e4d56
Manuel Sanchez <manuelsancheza@gmail.com>
2022-11-22 16:21:33 +0000

    whatever

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,7 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
 
   def new
     @booking = Booking.new
@@ -10,6 +13,26 @@ class BookingsController < ApplicationController
     @friend = Friend.find(params[:friend_id])
     @booking.friend = @friend
     redirect_to bookings_path(@booking)
+  end
+
+  def show
+    @booking = User.find(params[:id])
+  end
+
+  def destroy
+    @booking = User.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path, status: :see_other
+  end
+
+  def confirm
+    @booking.update(confirmed?: true)
+    redirect_to bookings_index_path, status: :see_other
+  end
+
+  def reject
+    @booking.update(confirmed?: nil)
+    redirect_to bookings_index_path, status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,13 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :users
-  resources :friends do
+  resources :users do
     resources :bookings, only: %i[index destroy]
   end
   resources :bookings, only: %i[new create update]
+
+  get 'bookings/index', to: 'bookings#index'
+  get 'bookings/:id/confirm', to: 'bookings#confirm'
+  get 'bookings/:id/reject', to: 'bookings#reject'
+  delete 'bookings/:id', to: 'bookings#destroy'
 end
