35aeb26781d3377faa2330fef7a4ffaf7510acb1
Manuel Sanchez <manuelsancheza@gmail.com>
2022-11-23 12:24:09 +0000

    Dropdown at sign-up and improved bookings index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
+  ROLE = %w[Customer Friend]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
