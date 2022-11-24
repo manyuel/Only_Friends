6ca3c4304752dee3eb820a15514504e622c0bd8d
Ammar Amin <ammaramin78@gmail.com>
2022-11-24 15:07:22 +0000

    seeding images

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,37 @@ Booking.destroy_all
 User.destroy_all
 
 puts "creating friends"
-User.create!(name: "Harry Potter", role: "Friend", rate: 320, location: "Surrey", rating: 4.8, bio: "Likes playing quidditch, long walks in the forbidden forests", email: "hello@email.com", password: "password123")
-User.create!(name: "Boris Johnson", role: "Friend", rate: 85, location: "Uxbridge", rating: 3.5, bio: "Likes making fun of poor people, tackling children and zip-lining across London", email: "bye@email.com", password: "password123")
-User.create!(name: "Theresa May", role: "Friend", rate: 32.50, location: "Wesminster", rating: 0.1, bio: "Likes running in fields of wheat, dislikes politics", email: "123@email.com", password: "password123")
-User.create!(name: "Professor Snape", role: "Friend", rate: 438.79, location: "Hackney", rating: 4.99, bio: "Professor of the dark arts and loves a good sunbathe", email: "whatever@gmial.com", password: "password123")
+harry = User.create!(name: "Harry Smith", role: "Friend", rate: 320, location: "Surrey", rating: 4.8, bio: "Likes playing football and a good boozer", email: "hello@email.com", password: "password123")
+abdul = User.create!(name: "Abdul Kareem", role: "Friend", rate: 85, location: "Uxbridge", rating: 3.5, bio: "I really enjoy baking and playing cricket", email: "bye@email.com", password: "password123")
+maggie = User.create!(name: "Maggie Poppy", role: "Friend", rate: 32.50, location: "Wesminster", rating: 0.1, bio: "Im Maggie, 3 times married and enjoy going to the cinema", email: "123@email.com", password: "password123")
+julia = User.create!(name: "Julia Jenkins", role: "Friend", rate: 438.79, location: "Hackney", rating: 4.99, bio: "Professional runner and loves a good sunbathe", email: "whatever@gmial.com", password: "password123")
+
+paulina = User.create!(name: "Paulina Rowling", role: "Friend", rate: 2.50, location: "Sweden", rating: 1.6, bio: "Im Maggie, 3 times married and enjoy going to the cinema", email: "paulina@email.com", password: "password123")
+pedro = User.create!(name: "Pedro Mendoza", role: "Friend", rate: 531.40, location: "Barcelona", rating: 4.20, bio: "Im a aspiring footballer and enjoy game-night", email: "pedror@gmial.com", password: "password123")
+
+file = open("app/assets/images/lady1.jpeg")
+julia.photo.attach(io: file, filename: "nes.png", content_type: "image/jpeg")
+julia.save
+
+file = open("app/assets/images/harry1.jpg")
+harry.photo.attach(io: file, filename: "nes.png", content_type: "image/jpeg")
+harry.save
+
+file = open("app/assets/images/lady.jpeg")
+maggie.photo.attach(io: file, filename: "nes.png", content_type: "image/jpeg")
+maggie.save
+
+file = open("app/assets/images/man.jpeg")
+abdul.photo.attach(io: file, filename: "nes.png", content_type: "image/jpeg")
+abdul.save
+
+file = open("app/assets/images/pedro1.jpg")
+pedro.photo.attach(io: file, filename: "nes.png", content_type: "image/jpeg")
+pedro.save
+
+file = open("app/assets/images/paulina.jpg")
+paulina.photo.attach(io: file, filename: "nes.png", content_type: "image/jpeg")
+paulina.save
 
 puts "creating costumers"
 User.create!(name: "Rick", role: "Costumer", rating: 1, bio: "Likes boardgames & videogames", email: "rick@email.com", password: "password123")
